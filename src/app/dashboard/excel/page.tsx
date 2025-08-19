ba99e8b7105068c3bded2fb22c8cb459001e095e
kishanshettyb <kishanqr@gmail.com>
2025-08-19 16:06:46 +0530

    feat: fix
    fix issues

--- src/app/dashboard/excel/page.tsx
+++ src/app/dashboard/excel/page.tsx
@@ -10,7 +10,7 @@ function page() {
 		<div>
 			<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
 				<SidebarTrigger className="-ml-1" />
-				<Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+				<Separator className="mr-2 data-[orientation=vertical]:h-4" />
 				<Breadcrumb>
 					<BreadcrumbList>
 						<BreadcrumbItem className="hidden md:block">
