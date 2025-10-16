2336e76368c0a3609d93f90e25b8ed34ff0f05b5
kishanshettyb <kishanqr@gmail.com>
2025-10-16 14:10:29 +0530

    fix issues

--- src/components/version-switcher.tsx
+++ src/components/version-switcher.tsx
@@ -19,7 +19,7 @@ export function VersionSwitcher({ versions, defaultVersion }: { versions: string
 								<GalleryVerticalEnd className="size-4" />
 							</div>
 							<div className="flex flex-col gap-0.5 leading-none">
-								<span className="font-medium">College Result Analysis</span>
+								<span className="font-medium">GEC Chamarajnagar</span>
 							</div>
 							<ChevronsUpDown className="ml-auto" />
 						</SidebarMenuButton>
--- src/services/api/googlesheeet/googlesheet.ts
+++ src/services/api/googlesheeet/googlesheet.ts
@@ -2,9 +2,9 @@ import { Googlesheet } from "@/types/googlesheet";
 import axios from "axios";
 
 const token =
-	"a7be0941b29ba54f3cb9de531510f14ef0114a9211fb3c2c305af6d14e5ee8cfb66f7d968dae98cefc0620101fe8015a398e07a09ab73013ef227404a12d9023ce230938920a853e7d6eb248759e034369dd5b84c549f830644ab78f82a0ce252cf64ba515d8b58edc4826491df0607ea8887ac8beaab691b50e820a56dfd6b9";
+	"19ad7bc3af029bc7eca668471185ba647a167f6946f864414c739db67afd9240d7add2d0b19b27af232dcc091eec15cf72714996fb017b05dd4b7d3d3c08981b88fb44eb87787c6f60d186ffb8ea97d59fdb644cfcd3ed9c802b13a8190176c81931bfb8a3ef6802084aaecb711a27782e35e646b2406ce5f7c0a0a2e8db1ebe";
 const axiosInstance = axios.create({
-	baseURL: "https://makemystatus-strapi.onrender.com/api/",
+	baseURL: "https://light-birds-a8f47896af.strapiapp.com/api/",
 
 	headers: {
 		Authorization: `Bearer ${token}`
