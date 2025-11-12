04f4b602d6f6326dc8b97cad2738e15c015c7aba
kishanshettyb <kishanqr@gmail.com>
2025-11-12 11:59:13 +0530

    fix issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,63 @@
 import { LoginForm } from "@/components/login-form";
-import { User } from "lucide-react";
+import { ShieldUser, User, Users } from "lucide-react";
 import Link from "next/link";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
 export default function Page() {
 	return (
 		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
 			<div className="w-full max-w-sm p-5">
-				<LoginForm title="Admin Login" />
-				<div className="my-5 text-center">
-					<Link href="/students/login" className="text-center text-blue-600 items-center flex gap-x-2 justify-center text-sm my-5">
-						<User size={16} />
-						Student Login
-					</Link>
-				</div>
+				<Tabs defaultValue="account">
+					<TabsList className="w-full">
+						<TabsTrigger value="account">
+							<ShieldUser />
+							Faculty
+						</TabsTrigger>
+						<TabsTrigger value="password">
+							<Users />
+							Students
+						</TabsTrigger>
+					</TabsList>
+					<TabsContent value="account">
+						<Card>
+							<CardHeader>
+								<CardTitle className="bg-slate-50 py-3 rounded-2xl">
+									<Image
+										src="/images/new-logo-gec.png"
+										width="1000"
+										height="1000"
+										className="w-[100px] flex justify-center items-center mx-auto  shadow-2xl shadow-blue-200 border-slate-100 h-[100px] object-cover rounded-full"
+										alt=""
+									/>
+									<h2 className="text-center text-xl mt-5 font-semibold">GEC-Chamarajanagara</h2>
+								</CardTitle>
+							</CardHeader>
+							<CardContent className="grid gap-6">
+								<LoginForm title="Faculty Login" />
+							</CardContent>
+						</Card>
+					</TabsContent>
+					<TabsContent value="password">
+						<Card>
+							<CardHeader>
+								<CardTitle>
+									<Image
+										src="/images/new-logo-gec.png"
+										width="1000"
+										height="1000"
+										className="w-[100px] flex justify-center items-center mx-auto  shadow-2xl shadow-blue-200 border-slate-100 h-[100px] object-cover rounded-full"
+										alt=""
+									/>
+									<h2 className="text-center text-xl mt-5 font-semibold">GEC-Chamarajanagara</h2>
+								</CardTitle>
+							</CardHeader>
+							<CardContent className="grid gap-6">
+								<LoginForm title="Student Login" />
+							</CardContent>
+						</Card>
+					</TabsContent>
+				</Tabs>
 			</div>
 		</div>
 	);
--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { useState } from "react";
 import { Button } from "./ui/button";
 import { useAdminLogin } from "@/services/mutation/user/user";
-import Image from "next/image";
 
 interface LoginFormProps extends React.ComponentProps<"div"> {
 	title: string;
@@ -39,49 +37,38 @@ export function LoginForm({ title, signup, className, ...props }: LoginFormProps
 
 	return (
 		<div className={cn("flex flex-col gap-6", className)} {...props}>
-			<Image
-				src="/images/new-logo-gec.png"
-				width="1000"
-				height="1000"
-				className="w-[200px] flex justify-center items-center mx-auto  shadow-2xl shadow-blue-200 border-slate-100 h-[200px] object-cover rounded-full"
-				alt=""
-			/>
-			<h2 className="text-center text-3xl font-semibold">GEC-Chamarajanagara</h2>
-			<Card>
-				<CardContent>
-					<div className="border border-x-0 border-t-0 pb-3">
-						<h2 className="text-center text-xl  font-semibold">{title}</h2>
-					</div>
-
-					<form onSubmit={handleSubmit} className="mt-5">
-						<div className="flex flex-col gap-6">
-							<div className="grid gap-3">
-								<Label htmlFor="email">Email or Username</Label>
-								<Input id="identifier" name="identifier" type="text" value={formData.identifier} onChange={handleInputChange} required />
-							</div>
-							<div className="grid gap-3">
-								<div className="flex flex-row justify-between items-center">
-									<Label htmlFor="password">Password</Label>
-								</div>
-								<Input id="password" name="password" type="password" value={formData.password} onChange={handleInputChange} required />
-							</div>
-							<div className="flex flex-col gap-3">
-								<Button type="submit" disabled={loginMutation.isPending}>
-									{loginMutation.isPending ? "Logging in..." : "Login"}
-								</Button>
-							</div>
+			<div>
+				<div className="pb-2">
+					<h2 className="text-center text-base font-semibold">{title}</h2>
+				</div>
+				<form onSubmit={handleSubmit} className="mt-0 p-4 border rounded-2xl">
+					<div className="flex flex-col gap-6">
+						<div className="grid gap-3">
+							<Label htmlFor="email">Email or Username</Label>
+							<Input id="identifier" name="identifier" type="text" value={formData.identifier} onChange={handleInputChange} required />
 						</div>
-						{signup && (
-							<div className="mt-4 text-center text-xs">
-								Don&apos;t have an account?{" "}
-								<Link href="/register" className="underline underline-offset-4">
-									Sign up
-								</Link>
+						<div className="grid gap-3">
+							<div className="flex flex-row justify-between items-center">
+								<Label htmlFor="password">Password</Label>
 							</div>
-						)}
-					</form>
-				</CardContent>
-			</Card>
+							<Input id="password" name="password" type="password" value={formData.password} onChange={handleInputChange} required />
+						</div>
+						<div className="flex flex-col gap-3">
+							<Button type="submit" disabled={loginMutation.isPending}>
+								{loginMutation.isPending ? "Logging in..." : "Login"}
+							</Button>
+						</div>
+					</div>
+					{signup && (
+						<div className="mt-4 text-center text-xs">
+							Don&apos;t have an account?{" "}
+							<Link href="/register" className="underline underline-offset-4">
+								Sign up
+							</Link>
+						</div>
+					)}
+				</form>
+			</div>
 		</div>
 	);
 }
